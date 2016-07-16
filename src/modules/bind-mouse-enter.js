687765f76f244c583251c9a307021e30f3a739e5
Joel Eisner <eisnerjr@vcu.edu>
2016-07-16 14:07:58 -0400

    Updated bind-mouse-enter.js to select element attributes with the new settings.selector prefix and to create the mousetip with the id of settings.selector

--- src/modules/bind-mouse-enter.js
+++ src/modules/bind-mouse-enter.js
@@ -4,17 +4,17 @@ this.bindMouseEnter = function(element, settings) {
     element.addEventListener('mouseenter', function(event) {
         // When the user enters the element, create the mouse-tip element with a message defined from the elements 'mousetip-msg' attribute, ...
         var mouseTip = document.createElement('span'),
-            elMsg = element.getAttribute('mousetip-msg'),
+            elMsg = element.getAttribute(settings.selector + '-msg'),
             mouseTipMsg = document.createTextNode(elMsg),
             // ... attempt to grab any styling attributes, ...
-            mouseTipZIndex = element.getAttribute('mousetip-css-zindex'),
-            mouseTipPosition = element.getAttribute('mousetip-css-position'),
-            mouseTipPadding = element.getAttribute('mousetip-css-padding'),
-            mouseTipBorderRadius = element.getAttribute('mousetip-css-borderradius'),
-            mouseTipBackground = element.getAttribute('mousetip-css-background'),
-            mouseTipColor = element.getAttribute('mousetip-css-color');
+            mouseTipZIndex = element.getAttribute(settings.selector + '-css-zindex'),
+            mouseTipPosition = element.getAttribute(settings.selector + '-css-position'),
+            mouseTipPadding = element.getAttribute(settings.selector + '-css-padding'),
+            mouseTipBorderRadius = element.getAttribute(settings.selector + '-css-borderradius'),
+            mouseTipBackground = element.getAttribute(settings.selector + '-css-background'),
+            mouseTipColor = element.getAttribute(settings.selector + '-css-color');
         // ... give the mouse-tip an id for easier DOM selection, ...
-        mouseTip.id = 'mousetip';
+        mouseTip.id = settings.selector;
         // ... set the default styles of the mouse-tip (use styling attributes if applicable), ...
         mouseTip.style.zIndex = mouseTipZIndex || settings.cssZIndex;
         mouseTip.style.position = mouseTipPosition || settings.cssPosition;
