3578cb3b4e9a7ae66fdca99928dfa467adc88dde
Joel Eisner <eisnerjr@vcu.edu>
2016-07-16 14:09:01 -0400

    Updated bind-mouse-leave.js to delete the mousetip element by the id of settings.selector

--- src/modules/bind-mouse-leave.js
+++ src/modules/bind-mouse-leave.js
@@ -1,9 +1,9 @@
 // Bind Mouse Leave Function
-this.bindMouseLeave = function(element) {
+this.bindMouseLeave = function(element, settings) {
     // Bind a 'mouseleave' event listener to the element
     element.addEventListener('mouseleave', function(event) {
         // When the user leaves the element, delete the mouse-tip element
-        var mouseTip = document.getElementById('mousetip');
+        var mouseTip = document.getElementById(settings.selector);
         mouseTip.parentNode.removeChild(mouseTip);
     });
 };
