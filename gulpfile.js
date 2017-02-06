132d796d62d76fd9c55c26545e20a4e5982424e9
Joel Eisner <jeisner93@gmail.com>
2017-02-05 22:58:53 -0500

    Updated the gulpfile.js to be simple, remove the clean task and its dependencies, and add some ES2015 (ES6) syntax-sugar

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,24 @@
-// Require
-var config = require('./gulp/config'),
-    gulp = require('gulp'),
-    clean = require('gulp-clean'),
-    header = require('./gulp/header'),
+let config =  require('./gulp/config'),
+    gulp =    require('gulp'),
+    header =  require('./gulp/header'),
     include = require('gulp-include'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+    rename =  require('gulp-rename'),
+    uglify =  require('gulp-uglify');
 
-// Default Task
+// Default
 gulp.task('default', ['watch']);
 
-// Watch Task
-gulp.task('watch', function() {
-    gulp.watch(config.watch, ['compile']);
-});
+// Watch
+gulp.task('watch', () => gulp.watch(config.watch, ['compile']));
 
-// Clean JS Task
-gulp.task('clean', function() {
-    return gulp.src(config.dest, {read:false})
-        .pipe(clean());
-});
-
-// Compile JS task
-gulp.task('compile', ['clean'], function() {
+// Compile
+gulp.task('compile', ['clean'], () => {
     return gulp.src(config.src)        // Input
         .pipe(include())               // Include modules
         .pipe(header())                // Header
         .pipe(gulp.dest(config.dest))  // Expanded output
         .pipe(uglify())                // Uglify
         .pipe(header(true))            // Header
-        .pipe(rename({suffix:'.min'}))
+        .pipe(rename({suffix:'.min'})) // Rename
         .pipe(gulp.dest(config.dest)); // Minified output
 });
