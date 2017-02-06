cae70839acadc05008174298b9e74532822e15a4
Joel Eisner <jeisner93@gmail.com>
2017-02-05 23:07:32 -0500

    Updated the header.js gulp module to use ES2015 (ES6) syntax

--- gulp/header.js
+++ gulp/header.js
@@ -1,21 +1,20 @@
-var header = require('gulp-header'),
-    pkg = require('../package.json');
+let header = require('gulp-header'),
+    pkg =    require('../package.json');
 
-module.exports = function(min) {
-    var bannerExp = [
-        '/**',
-        ' * <%= pkg.name %> - <%= pkg.description %>',
-        ' * @version <%= pkg.version %>',
-        ' * @author <%= pkg.author %>',
-        ' * @link <%= pkg.homepage %>',
-        ' * @license <%= pkg.license %>',
-        ' */',
-        '',
-        ''
-    ].join('\n');
-    var bannerMin = [
-        '/* <%= pkg.name %> <%= pkg.version %> | <%= pkg.license %> | <%= pkg.homepage %> */',
-        ''
-    ].join('\n');
-    return min ? header(bannerMin, {pkg:pkg}) : header(bannerExp, {pkg:pkg});
+module.exports = min => {
+    let bannerExp =
+`/**
+ * <%= pkg.name %> - <%= pkg.description %>
+ * @version <%= pkg.version %>
+ * @author <%= pkg.author %>
+ * @link <%= pkg.homepage %>
+ * @license <%= pkg.license %>
+ */
+
+`,
+    bannerMin =
+`/* <%= pkg.name %> <%= pkg.version %> | <%= pkg.license %> | <%= pkg.homepage %> */
+
+`;
+    return min ? header(bannerMin, {pkg}) : header(bannerExp, {pkg});
 };
