03d9ae1b08f1bcd2e452691f797614bf249c2caa
Joel Eisner <jeisner93@gmail.com>
2018-01-17 21:45:49 -0500

    Temporarily disabled JS minification in the Gulp build-system while working with the ES2015 update of the main script

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const config =  require('./gulp/config');
 const gulp =    require('gulp');
 const header =  require('./gulp/header');
-const rename =  require('./gulp/rename');
-const uglify =  require('gulp-uglify');
+// const rename =  require('./gulp/rename');
+// const uglify =  require('gulp-uglify');
 
 gulp.task('build', () => {
     gulp.src(config.src)               // Input
         .pipe(header())                // Header
-        .pipe(gulp.dest(config.dest))  // Expanded output
-        .pipe(uglify())                // Uglify
-        .pipe(header(true))            // Header
-        .pipe(rename())                // Rename
-        .pipe(gulp.dest(config.dest)); // Minified output
+        .pipe(gulp.dest(config.dest)); // Expanded output
+    // .pipe(uglify())                // Uglify
+    // .pipe(header(true))            // Header
+    // .pipe(rename())                // Rename
+    // .pipe(gulp.dest(config.dest)); // Minified output
 });
 
 gulp.task('watch', () => gulp.watch(config.watch, ['build']));
