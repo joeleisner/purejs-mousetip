9432f1e8881f896422e312e3dfa2afe985baffa8
Joel Eisner <jeisner93@gmail.com>
2018-01-17 21:47:58 -0500

    Updated the main script to utilize an ES2015 class structure (as opposed to a regular constructor function), renamed the run() method to start(), added a stop() method, and restructured the internals for optimization/readability

--- src/mousetip.js
+++ src/mousetip.js
@@ -1,149 +1,175 @@
-function MouseTip(userSettings) {
-    // Select all elements with the attribute 'mousetip' (or user-specified selector), and accept user-defined render settings or constructor defaults
-    var config = userSettings || {},
-        settings = {
-            cssZIndex: config.cssZIndex || '9999',
-            cssPosition: config.cssPosition || 'absolute',
-            cssPadding: config.cssPosition || '15px',
-            cssBorderRadius: config.cssPosition || '4px',
-            cssBackground: config.cssBackground || 'rgba(0,0,0,0.75)',
-            cssColor: config.cssColor || '#fff',
-            html: config.html || true,
-            position: config.position || 'bottom right',
-            selector: config.selector || 'mousetip'
-        };
+class MouseTip {
 
-    // Run Function
-    this.run = function() {
-        // Select all elements...
-        var elements = document.querySelectorAll('[' + settings.selector + ']');
-        // ... and for each...
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            // ... bind it to events triggered by 'mouseenter', 'mouseleave', and 'mousemove'
-            this.bindMouseEnter(element, settings);
-            this.bindMouseLeave(element, settings);
-            this.bindMouseMove(element, settings);
+    // Construct the class
+    constructor(settings) {
+        // Define default settings if not provided...
+        const {
+            cssZIndex        = '9999',
+            cssPosition      = 'absolute',
+            cssPadding       = '15px',
+            cssBorderRadius  = '4px',
+            cssBackground    = 'rgba(0,0,0,0.75)',
+            cssColor         = '#fff',
+            html             = true,
+            position         = 'bottom right',
+            selector         = 'mousetip'
+        } = settings || {};
+        // ... and assign them to the class
+        this.cssZIndex       = cssZIndex;
+        this.cssPosition     = cssPosition;
+        this.cssPadding      = cssPadding;
+        this.cssBorderRadius = cssBorderRadius;
+        this.cssBackground   = cssBackground;
+        this.cssColor        = cssColor;
+        this.html            = html;
+        this.position        = position;
+        this.selector        = selector;
+        // Store the state of the mousetip
+        this.mouseTip = false;
+    }
+
+    // Create the mousetip
+    createMouseTip(event) {
+        // Create the mousetip
+        const mouseTip = document.createElement('span');
+        // Store the styling either from the target element's attributes or the constructor settings
+        const zIndex       = event.target.getAttribute(this.selector + '-css-zindex')       || this.cssZIndex;
+        const position     = event.target.getAttribute(this.selector + '-css-position')     || this.cssPosition;
+        const padding      = event.target.getAttribute(this.selector + '-css-padding')      || this.cssPadding;
+        const borderRadius = event.target.getAttribute(this.selector + '-css-borderradius') || this.cssBorderRadius;
+        const background   = event.target.getAttribute(this.selector + '-css-background')   || this.cssBackground;
+        const color        = event.target.getAttribute(this.selector + '-css-color')        || this.cssColor;
+        // Assign the ID and styling to the mousetip
+        mouseTip.id                 = this.selector;
+        mouseTip.style.zIndex       = zIndex;
+        mouseTip.style.position     = position;
+        mouseTip.style.padding      = padding;
+        mouseTip.style.borderRadius = borderRadius;
+        mouseTip.style.background   = background;
+        mouseTip.style.color        = color;
+        // Grab the message and HTML attributes from the event target
+        const message  = event.target.getAttribute(this.selector + '-msg');
+        const html     = this.html ?
+            event.target.hasAttribute(this.selector + '-disable-html') :
+            event.target.hasAttribute(this.selector + '-enable-html');
+        // If HTML is disabled globally and on the target element (or the inverse)...
+        if ((!this.html && !html) || (this.html && html)) {
+            // ... append the message to the mousetip as a text-node...
+            mouseTip.appendChild(document.createTextNode(message));
+        } else {
+            // ... otherwise, append the message to the mousetip as HTML
+            mouseTip.innerHTML = message;
         }
-    };
+        // Append the mousetip to the bottom of the page...
+        document.body.appendChild(mouseTip);
+        // ... and update the constructor's mousetip state
+        this.mouseTip = true;
+    }
 
-    // Bind Mouse Enter Function
-    this.bindMouseEnter = function(element, settings) {
-        // Bind a 'mouseenter' event listener to the element
-        element.addEventListener('mouseenter', function(event) {
-            // When the user enters the element, create the mouse-tip element with a message defined from the elements 'mousetip-msg' attribute, ...
-            var mouseTip = document.createElement('span'),
-                elMsg = element.getAttribute(settings.selector + '-msg'),
-                mouseTipMsg = document.createTextNode(elMsg),
-                // ... toggle which HTML attribute to look for based on the global settings, ...
-                mouseTipHTML = settings.html ? element.hasAttribute(settings.selector + '-disable-html') : element.hasAttribute(settings.selector + '-enable-html'),
-                // ... attempt to grab any styling attributes, ...
-                mouseTipZIndex = element.getAttribute(settings.selector + '-css-zindex'),
-                mouseTipPosition = element.getAttribute(settings.selector + '-css-position'),
-                mouseTipPadding = element.getAttribute(settings.selector + '-css-padding'),
-                mouseTipBorderRadius = element.getAttribute(settings.selector + '-css-borderradius'),
-                mouseTipBackground = element.getAttribute(settings.selector + '-css-background'),
-                mouseTipColor = element.getAttribute(settings.selector + '-css-color');
-            // ... give the mouse-tip an id for easier DOM selection, ...
-            mouseTip.id = settings.selector;
-            // ... set the default styles of the mouse-tip (use styling attributes if applicable), ...
-            mouseTip.style.zIndex = mouseTipZIndex || settings.cssZIndex;
-            mouseTip.style.position = mouseTipPosition || settings.cssPosition;
-            mouseTip.style.padding = mouseTipPadding || settings.cssPadding;
-            mouseTip.style.borderRadius = mouseTipBorderRadius || settings.cssBorderRadius;
-            mouseTip.style.background = mouseTipBackground || settings.cssBackground;
-            mouseTip.style.color = mouseTipColor || settings.cssColor;
-            // ... if HTML is disabled globally and not enabled via attribute, or enabled globally and disabled via attribute...
-            if ((!settings.html && !mouseTipHTML) || (settings.html && mouseTipHTML)) {
-                // ... append the message to the mouse-tip as a text-node...
-                mouseTip.appendChild(mouseTipMsg);
-            } else {
-                // ... otherwise, append the message to the mouse-tip as HTML...
-                mouseTip.innerHTML = elMsg;
-            }
-            // ... and append the mouse-tip to the document body
-            document.body.appendChild(mouseTip);
-        });
-    };
+    // Delete the mousetip
+    deleteMouseTip() {
+        // If the constructor's mousetip state is not true, return
+        if (!this.mouseTip) return;
+        // Grab the mousetip,...
+        const mouseTip = document.getElementById(this.selector);
+        // ... delete it,...
+        mouseTip.parentNode.removeChild(mouseTip);
+        // ... and update the constructor's mousetip state
+        this.mouseTip = false;
+    }
 
-    // Bind Mouse Leave Function
-    this.bindMouseLeave = function(element, settings) {
-        // Bind a 'mouseleave' event listener to the element
-        element.addEventListener('mouseleave', function(event) {
-            // When the user leaves the element, delete the mouse-tip element
-            var mouseTip = document.getElementById(settings.selector);
-            mouseTip.parentNode.removeChild(mouseTip);
-        });
-    };
+    // Update the mousetip
+    updateMouseTip(event) {
+        // Grab the mousetip
+        const mouseTip = document.getElementById(this.selector);
+        // Grab the X/Y of the mouse
+        const mouseX = event.pageX;
+        const mouseY = event.pageY;
+        // Set the default adjustment to 15
+        const defaultAdjust = 15;
+        // Get the mousetip position from the target element or the constructor
+        let position = (event.target.getAttribute(this.selector + '-pos') || this.position).split(' '),
+            verticalAdjust, horizontalAdjust;
+        // If the position does not contain two items, set it to the default
+        if (position.length !== 2) position = ['bottom', 'right'];
+        // Set the vertical adjustment from the first item of position
+        switch(position[0]) {
+        case 'top':
+            verticalAdjust = -defaultAdjust - mouseTip.offsetHeight;
+            break;
+        case 'center':
+            verticalAdjust = 0 - (mouseTip.offsetHeight / 2);
+            break;
+        default:
+            verticalAdjust = defaultAdjust;
+        }
+        // Set the horizontal adjustment from the second item of position
+        switch(position[1]) {
+        case 'left':
+            horizontalAdjust = -defaultAdjust - mouseTip.offsetWidth;
+            break;
+        case 'center':
+            horizontalAdjust = 0 - (mouseTip.offsetWidth / 2);
+            break;
+        default:
+            horizontalAdjust = defaultAdjust;
+        }
+        // Update the mousetip's position
+        mouseTip.style.top  = `${ mouseY + verticalAdjust }px`;
+        mouseTip.style.left = `${ mouseX + horizontalAdjust }px`;
+    }
 
-    // Bind Mouse Move Function
-    this.bindMouseMove = function(element, settings) {
-        // Bind a 'mousemove' event listener to the element
-        element.addEventListener('mousemove', function(event) {
-            // When the user moves within the element, get the user's mouse X and Y, ...
-            var mouseX = event.pageX,
-                mouseY = event.pageY,
-                // ... select the mouse-tip, ...
-                mouseTip = document.getElementById(settings.selector),
-                // ... then check to see if the element has a specified mousetip position
-                mouseTipPos = element.getAttribute(settings.selector + '-pos') || settings.position;
-            // Split the mousetip position value into a two part array...
-            var posArray = mouseTipPos.split(' '),
-                vertAdj, horizAdj;
-            // If there are two items in the position array...
-            if (posArray.length == 2) {
-                // For the first item of the array (vertical position), ...
-                switch (posArray[0]) {
-                    // ... if it's 'bottom'...
-                    case 'bottom':
-                        // ... set vertAdj to 15, ...
-                        vertAdj = 15;
-                        break;
-                    // ... if it's 'center'...
-                    case 'center':
-                        // ... set vertAdj to 0 minus half of the mousetip's height, ...
-                        vertAdj = 0 - (mouseTip.offsetHeight / 2);
-                        break;
-                    // ... if it's 'top'...
-                    case 'top':
-                        // ... set vertAdj to -15 minues the mousetip's height, ...
-                        vertAdj = -15 - mouseTip.offsetHeight;
-                        break;
-                    // ... otherwise, ...
-                    default:
-                        // ... set vertAdj to 15 (Default: 'bottom')
-                        vertAdj = 15;
-                }
-                // For the second item in the array (horizontal position), ...
-                switch (posArray[1]) {
-                    // ... if it's 'right'...
-                    case 'right':
-                        // ... set horizAdj to 15, ...
-                        horizAdj = 15;
-                        break;
-                    // ... if it's 'center'...
-                    case 'center':
-                        // ... set horizAdj to 0 minus half of the mousetip's width, ...
-                        horizAdj = 0 - (mouseTip.offsetWidth / 2);
-                        break;
-                    // ... if it's 'left'...
-                    case 'left':
-                        // ... set horizAdj to -15 minus the mousetip's width
-                        horizAdj = -15 - mouseTip.offsetWidth;
-                        break;
-                    // ... otherwise, ...
-                    default:
-                        // ... set horizAdj to 15 (Default: 'right')
-                        horizAdj = 15;
-                }
-            } else {
-                // ... otherwise, set the adjustment variables to 15 and 15 (Default: 'bottom right')
-                vertAdj = 15;
-                horizAdj = 15;
-            }
-            // ... then set the mousetip's top and left based on the mouse's X and Y plus the vertical and horizontal adjustments
-            mouseTip.style.top = mouseY + vertAdj + 'px';
-            mouseTip.style.left = mouseX + horizAdj + 'px';
-        });
-    };
+    // Handle mouse events
+    handleEvent(event) {
+        switch (event.type) {
+        case 'mouseenter':
+            // When the mouse enters an element, create the mousetip
+            this.createMouseTip(event);
+            break;
+        case 'mouseleave':
+            // When the mouse leaves an element, delete the mousetip
+            this.deleteMouseTip();
+            break;
+        case 'mousemove':
+            // When the mouse moves inside an element, update the mousetip
+            this.updateMouseTip(event);
+        }
+    }
+
+    // Start handling mouse events
+    start() {
+        // Grab all elements by selector
+        const elements = document.querySelectorAll(`[${ this.selector }]`);
+        // If no elements were found, return
+        if (!elements) return;
+        // Store the elements for reference,...
+        this.elements = elements;
+        // ... and for each,...
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            // ... bind the mouse enter, leave, and move events
+            element.addEventListener('mouseenter', this, false);
+            element.addEventListener('mouseleave', this, false);
+            element.addEventListener('mousemove',  this, false);
+        }
+    }
+
+    // Stop handling mouse events
+    stop() {
+        // If no element references are stored, return
+        if (!this.elements || !this.elements.length) return;
+        // For each element...
+        for (let i = 0; i < this.elements.length; i++) {
+            const element = this.elements[i];
+            // ... unbind the mouse enter, leave, and move events
+            element.removeEventListener('mouseenter', this, false);
+            element.removeEventListener('mouseleave', this, false);
+            element.removeEventListener('mousemove',  this, false);
+        }
+        // Delete the stored element references...
+        delete this.elements;
+        // ... and the mousetip
+        this.deleteMouseTip();
+    }
+
 }
