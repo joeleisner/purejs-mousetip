4351b045cd2bae55845aa7cbe1eceddf5909aef6
Joel Eisner <jeisner93@gmail.com>
2018-08-12 18:08:50 -0400

    Updated the build system to work with gulp 4.0 and to compartmentalize the code

---
+++ gulp/tasks.js
@@ -0,0 +1,50 @@
+const babel  = require('gulp-babel'),
+    config   = require('./config'),
+    gulp     = require('gulp'),
+    header   = require('./header'),
+    inject   = require('gulp-inject-string'),
+    rename   = require('./rename'),
+    uglify   = require('gulp-uglify'),
+    uglifyes = require('gulp-uglifyes');
+
+function buildScript(type) {
+    const { dest, src } = config;
+    return gulp.src(src)
+        .pipe(type ? rename('.' + type) : babel())
+        .pipe(header())
+        .pipe(gulp.dest(dest))
+        .pipe(type ? uglifyes() : uglify())
+        .pipe(header(true))
+        .pipe(rename())
+        .pipe(gulp.dest(dest));
+}
+
+function buildModule(type) {
+    const { modules, src } = config,
+        { append, dest }   = modules;
+    return gulp.src(src)
+        .pipe(rename('.' + type))
+        .pipe(inject.append(append[type]))
+        .pipe(gulp.dest(dest));
+}
+
+module.exports = {
+    all() {
+        const { es2015, legacy, modules } = this;
+        return gulp.series(gulp.parallel(es2015, legacy, modules.all()));
+    },
+    es2015() { return buildScript('es2015'); },
+    legacy() { return buildScript(); },
+    modules: {
+        all() {
+            const { commonJs, es2015 } = this;
+            return gulp.series(gulp.parallel(commonJs, es2015));
+        },
+        commonJs() { return buildModule('common'); },
+        es2015()   { return buildModule('es2015'); }
+    },
+    watch() {
+        const { all } = this;
+        gulp.watch(config.watch, all());
+    }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,11 @@
-const babel    = require('gulp-babel');
-const config   = require('./gulp/config');
-const gulp     = require('gulp');
-const header   = require('./gulp/header');
-const inject   = require('gulp-inject-string');
-const rename   = require('./gulp/rename');
-const uglify   = require('gulp-uglify');
-const uglifyes = require('gulp-uglifyes');
+const gulp = require('gulp'),
+    tasks  = require('./gulp/tasks');
 
-gulp.task('build-legacy', () => {
-    gulp.src(config.src)
-        .pipe(babel())
-        .pipe(header())
-        .pipe(gulp.dest(config.dest))
-        .pipe(uglify())
-        .pipe(header(true))
-        .pipe(rename())
-        .pipe(gulp.dest(config.dest));
-});
-
-gulp.task('build-es2015', () => {
-    gulp.src(config.src)
-        .pipe(header())
-        .pipe(rename('.es2015'))
-        .pipe(gulp.dest(config.dest))
-        .pipe(uglifyes())
-        .pipe(header(true))
-        .pipe(rename())
-        .pipe(gulp.dest(config.dest));
-});
-
-gulp.task('build-commonjs-module', () => {
-    gulp.src(config.src)
-        .pipe(rename('.common'))
-        .pipe(inject.append(config.modules.append.commonjs))
-        .pipe(gulp.dest(config.modules.dest));
-});
-
-gulp.task('build-es2015-module', () => {
-    gulp.src(config.src)
-        .pipe(rename('.es2015'))
-        .pipe(inject.append(config.modules.append.es2015))
-        .pipe(gulp.dest(config.modules.dest));
-});
-
-gulp.task('build-modules', ['build-commonjs-module', 'build-es2015-module']);
-
-gulp.task('build', ['build-legacy', 'build-es2015', 'build-modules']);
-
-gulp.task('watch', () => gulp.watch(config.watch, ['build']));
-
-gulp.task('default', ['watch']);
+gulp.task('build-es2015',                    tasks.es2015);
+gulp.task('build-legacy',                    tasks.legacy);
+gulp.task('build',                            tasks.all());
+gulp.task('build-commonjs-module', tasks.modules.commonJs);
+gulp.task('build-es2015-module',     tasks.modules.es2015);
+gulp.task('build-modules',            tasks.modules.all());
+gulp.task('default',                          tasks.watch);
+gulp.task('watch',                            tasks.watch);
