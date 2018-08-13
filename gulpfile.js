0b897b24d498b2aea99032d9f22e4d0227748fea
Joel Eisner <jeisner93@gmail.com>
2018-08-12 21:35:39 -0400

    Fixed an issue where the watch task did not work due to this binding issues when piped through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,5 +7,5 @@ gulp.task('build',                            tasks.all());
 gulp.task('build-commonjs-module', tasks.modules.commonJs);
 gulp.task('build-es2015-module',     tasks.modules.es2015);
 gulp.task('build-modules',            tasks.modules.all());
-gulp.task('default',                          tasks.watch);
-gulp.task('watch',                            tasks.watch);
+gulp.task('default',                        tasks.watch());
+gulp.task('watch',                          tasks.watch());
