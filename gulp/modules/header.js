521bcb8b2cb45e5c1dc58d224d3b70ec50d73b7e
Joel Eisner <jeisner93@gmail.com>
2020-02-05 19:35:17 -0500

    Updated the Gulp header module to ensure header comments are not deleted (easily) by using a "!" in the expanded/minified header comment templates.

--- gulp/modules/header.js
+++ gulp/modules/header.js
@@ -1,7 +1,7 @@
 const header = require('gulp-header'),
     pkg      = require('../../package.json'),
     exp      =
-`/**
+`/*!
  * <%= pkg.name %> - <%= pkg.description %>
  * @version <%= pkg.version %>
  * @author <%= pkg.author %>
@@ -11,7 +11,7 @@ const header = require('gulp-header'),
 
 `,
     min      =
-`/* <%= pkg.name %> <%= pkg.version %> | <%= pkg.license %> | <%= pkg.homepage %> */
+`/*! <%= pkg.name %> <%= pkg.version %> | <%= pkg.license %> | <%= pkg.homepage %> */
 `;
 
 module.exports = type => header(type === 'min' ? min : exp, { pkg });
